633b13e00e4bb2782237a6dd6a867d2d5ace69bd
Sam White <sam.white745@gmail.com>
2023-05-24 11:11:41 +0100

    extra test for transaction

--- transaction.test.js
+++ transaction.test.js
@@ -6,4 +6,9 @@ describe('transaction', () => {
     const transaction = new Transaction({date:"14/01/2023", debit:"500.00", balance: "2500.00"});
     expect(transaction.print()).toEqual("14/01/2023 ||  || 500.00 || 2500.00");
   });
+
+  it('create a transaction with credit', () => {
+    const transaction = new Transaction({date:"14/01/2023", credit:"500.00", balance: "2000.00"});
+    expect(transaction.print()).toEqual("14/01/2023 || 500.00 ||  || 2000.00");
+  });
 });
